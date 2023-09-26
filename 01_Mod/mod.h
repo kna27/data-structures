74cc434455760349a174cea6a88f04827a5efbba
Krish Arora <krisharora27@gmail.com>
2023-09-26 15:25:06 -0400

    Add lab 1 header file

---
+++ 01_Mod/mod.h
@@ -0,0 +1,46 @@
+#ifndef ____Mod__
+#define ____Mod__
+#include <iostream>
+#include <cassert>
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::istream;
+using std::ostream;
+class Mod
+{
+public:
+    Mod(long t);
+    Mod(const Mod &m);
+    Mod &operator=(const Mod &m);
+    Mod &operator+=(const Mod &m);
+    Mod &operator-=(const Mod &m);
+    Mod &operator*=(const Mod &m);
+    Mod &operator/=(const Mod &m);
+    Mod operator-() const;
+    Mod pwr(long e) const;
+    long val() const;
+    static void set_modulus(long m);
+    static long get_modulus() { return modulus; }
+
+private:
+    long x;
+    static long modulus;
+    static Mod inv(long r0);
+};
+Mod operator+(const Mod &a, const Mod &b);
+Mod operator+(long t, const Mod &m);
+Mod operator-(const Mod &a, const Mod &b);
+Mod operator-(long t, const Mod &m);
+Mod operator*(const Mod &a, const Mod &b);
+Mod operator*(long t, const Mod &m);
+Mod operator/(const Mod &a, const Mod &b);
+Mod operator/(long t, const Mod &m);
+bool operator==(const Mod &a, const Mod &b);
+bool operator==(long t, const Mod &m);
+bool operator!=(const Mod &a, const Mod &b);
+bool operator!=(long t, const Mod &m);
+istream &operator>>(istream &is, Mod &m);
+ostream &operator<<(ostream &os, const Mod &m);
+// long Mod::modulus = 17;
+#endif /* defined(____Mod__) */
